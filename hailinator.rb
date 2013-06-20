fdbb495cd5a8515f1d92a086d8d3b4084cc53200
MakerSquare Student <student@challenge-5.att.net>
2013-06-20 14:49:17 -0500

    added twitter favorite and follow code to hailanator file

--- hailinator.rb
+++ hailinator.rb
@@ -1,18 +1,24 @@
 require "twitter"
 require "csv"
+# require "koala"  #started to install facebook gem, 'koala'
 
 Twitter.configure do |config|
-  config.consumer_key = "utPDH8BWHvZwrsaSbqM1Q"
-  config.consumer_secret = "0ocFufU1qB3tTMIRasG7EpLLlTsedncJ9f8GESPQg4"
-  config.oauth_token = "18338282-mahYki2gshIYSqhapADB3jSvK4R7fGm3iR88HdGvy"
-  config.oauth_token_secret = "bveLpx0S3pOu5RdYmtklqQJT5HDxDSmyMinJqW5IHM8"
+  config.consumer_key = "nLpqmlHLSERG1vJSXPs47Q"
+  config.consumer_secret = "O8cqNAUAy5PhHcokQPKP1XO5wbFrJF9thZMEAnX7Qk"
+  config.oauth_token = "18338282-rG1s7FFqHCAHPMVb8Co6PC3A5pegsXL8gd7nt8aa4"
+  config.oauth_token_secret = "PDjKeSy3TIXZK84WIMHWvLN4DxDyJily7WT4Raw8Ng"
 end
 
 
 
 CSV.open("hail.csv", "wb") do |csv| #test with ab
   csv << ["handle", "text", "url"]
-  Twitter.search("#haildamage", :count => 30, :result_type => "recent").results.map do |status|
-  csv << ["#{status.from_user}", "#{status.text}"]
+  Twitter.search("#GoSpursGo", :count => 1, :result_type => "recent").results.map do |status|
+  csv << ["#{status.from_user}", "#{status.text}"]  
+  # Twitter.follow(status.from_user) 
+  # Twitter.favorite(status.id)
+
+  # Koala. 
+    
   end
 end
